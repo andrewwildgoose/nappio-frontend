03dc29e9a2ebfca1b3c608c81ee81c16032cce90
andrewwildgoose <a.wildgoose@yahoo.co.uk>
2025-03-14 13:52:08 +0000

    Started work on homepage. Added colour palette.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,46 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import { onMount } from 'svelte';
+
+    let animate = false;
+
+    onMount(() => {
+        setTimeout(() => {
+            animate = true;
+        }, 500); // Small delay before animation starts
+    });
+</script>
+
+<style>
+    @keyframes fade-in {
+        from { opacity: 0; }
+        to { opacity: 1; }
+    }
+
+    @keyframes move-up {
+        from { transform: translateY(300px); }
+        to { transform: translateY(10px); }
+    }
+
+    .fade-in {
+        animation: fade-in 1s ease-out forwards;
+        transform: translateY(300px);
+    }
+
+    .move-up {
+        animation: move-up 1.5s ease-out forwards;
+        animation-delay: 1.5s; /* Delay to allow fade-in first */
+    }
+</style>
+
+<div class="flex flex-col items-center justify-center">
+    <div class="h-screen w-full p-10 bg-teal">
+        <div class="flex flex-col items-center justify-center">
+            <h1 
+                class="font-ranchers text-6xl text-warm-mustard absolute fade-in"
+                class:move-up={animate}
+            >
+                nappio
+            </h1>              
+        </div>
+    </div>
+</div>
