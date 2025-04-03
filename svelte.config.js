211aedfbdcbd98236ff2b178c2e441a6da29f05f
andrewwildgoose <a.wildgoose@yahoo.co.uk>
2025-04-03 11:59:47 +0100

    Added netlify.toml and netlify adapter for build config.

--- svelte.config.js
+++ svelte.config.js
@@ -3,13 +3,22 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+    // Consult https://svelte.dev/docs/kit/integrations
+    // for more information about preprocessors
+    preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: adapter()
-	}
+    kit: {
+        adapter: adapter({
+            // if true, will create a Netlify Edge Function rather
+            // than using standard Node-based functions
+            edge: false,
+
+            // if true, will split your app into multiple functions
+            // instead of creating a single one for the entire app.
+            // if `edge` is true, this option cannot be used
+            split: false
+        })
+    }
 };
 
 export default config;
